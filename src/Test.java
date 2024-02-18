a2b6d037e3cb10e02902903a2b969aca886c3f56
marcelgaiser <MarcelG1235@gmail.com>
2024-02-18 13:00:20 +0100

    Syntax und Struktur

---
+++ src/Test.java
@@ -0,0 +1,19 @@
+public class Test {
+    public static void test(String[] args){
+        System.out.println("Bad World!");
+    }
+    public static void main(String[] args){
+        System.out.println("Hello World!"); // Test
+        // Test 2
+        System.out.println("Bad World!");
+    }
+}
+
+class Car{
+    public void drive(){
+
+    }
+    public void brake(){
+
+    }
+}
