52697664ee603632dbd444d895506907e5f0399f
marcelgaiser <MarcelG1235@gmail.com>
2024-02-25 09:15:51 +0100

    Variablen und Datentypen

--- src/Test.java
+++ src/Test.java
@@ -1,19 +1,20 @@
 public class Test {
-    public static void test(String[] args){
-        System.out.println("Bad World!");
-    }
     public static void main(String[] args){
-        System.out.println("Hello World!"); // Test
-        // Test 2
-        System.out.println("Bad World!");
-    }
-}
+// Numbers
+        byte age = 22; // -128 - 127
+        short population = 200; // -32768 - 32767
+        int distance = 33000; // Range: -2^31 - 2^31-1
+        long big = 7000000L;
+// Floating point numbers
+        float lon = 65.5f;
+        double lat = 1.75;
+// Bool
+        boolean isJavaFun = true;
+// Character
+        char letter = 'A';
+// Reference
+        String message = "Hallo World ! 123";
 
-class Car{
-    public void drive(){
-
-    }
-    public void brake(){
-
+        System.out.println(message + age);
     }
 }
