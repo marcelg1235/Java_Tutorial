b01fc6be1b45719612d9393960d1adaba99c941e
marcelgaiser <MarcelG1235@gmail.com>
2024-03-03 11:52:24 +0100

    Arithmetische Operatoren und Verzweigungen

--- src/Test.java
+++ src/Test.java
@@ -1,20 +1,63 @@
 public class Test {
     public static void main(String[] args){
-// Numbers
-        byte age = 22; // -128 - 127
-        short population = 200; // -32768 - 32767
-        int distance = 33000; // Range: -2^31 - 2^31-1
-        long big = 7000000L;
-// Floating point numbers
-        float lon = 65.5f;
-        double lat = 1.75;
-// Bool
-        boolean isJavaFun = true;
-// Character
-        char letter = 'A';
-// Reference
-        String message = "Hallo World ! 123";
+        int num1 = 4;
+        int num2 = 2;
+        int num3 = 3;
 
-        System.out.println(message + age);
+        //Arithmetic operators
+        int sum = num1 + num2;
+        int diff = num1 - num2;
+        int prod = num1 * num2;
+        int quot = num1 / num3;
+        int rem = num1 % num3;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + diff);
+        System.out.println("Product: " + prod);
+        System.out.println("Quotient: " + quot);
+        System.out.println("Remainder: " + rem);
+
+        //Comparison operators
+        /*
+            ==
+            !=
+            <
+            <=
+            >
+            >=
+         */
+
+        //Logical operators
+        /*
+            &&
+            ||
+            !
+         */
+
+        int age = 24;
+        boolean isStudent = true;
+
+        if (age == 24 || !(isStudent == false)){
+            System.out.println("First if");
+        } else if (age == 25){
+            System.out.println("Second if");
+        } else {
+            System.out.println("Else");
+        }
+
+        //Strings
+        String st1 = "Hello";
+        String st2 = "Hel";
+        st2 += "lo";
+
+        System.out.println("STR1: " +st1);
+        System.out.println("STR2: " +st2);
+
+        if (st1.equals(st2)){
+            System.out.println("Equal");
+        } else {
+            System.out.println("NOP");
+        }
+
     }
 }
